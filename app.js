53b2b05c627944678fc1614e029be0c923026dce
Sanyam Bhaskar <sanyambhaskar5@gmail.com>
2020-05-23 23:03:50 +0530

    Random Function
    Added Random Function and also added a timer of 60 seconds in the game to make it even harder thus more fun.

--- app.js
+++ app.js
@@ -1,10 +1,47 @@
+var time = 60;
+function starttimer(){
+    setInterval(tplus, 1000);
+    function tplus(){
+        time--;
+        if(time==0){
+            alert('Out Of Time!!');
+            location.reload();
+        }
+        document.getElementsByClassName('timer')[0].innerHTML="Remaining Time: "+ time;
+        if(time<=10){document.getElementsByClassName('timer')[0].style.color = "red";}
+       
+    }
+
+}
+
+
 
 $("#start").click(function (){
     $('.intro').fadeOut();
     $("#cardbox").fadeIn();
-
+    starttimer();
 
 });
+
+function remarray(arr, index){
+    for(var l=index;l<arr.length-1;l++){
+        arr[l]=arr[l+1];
+    }
+    arr.pop();
+}
+var cardvals  = document.getElementsByClassName('card');
+var nums = [1, 2, 3, 4, 5, 6,7 , 8, 1, 2, 3, 4, 5, 6, 7, 8];
+
+for(var o=0;o<16;o++){
+    var rand = Math.floor(Math.random()*nums.length); 
+    cardvals[o].children[0].innerHTML = nums[rand];
+    remarray(nums, rand);
+}
+
+
+
+
+
 
 var counter = 0;
 var response  = [];
@@ -40,14 +77,14 @@ window.addEventListener('click', function (e) {
 
     var x = document.getElementsByClassName('done');
     if(x.length==16){
-        alert("You Completed The game with "+(8-counter) +" guesses remaining!!" );
+        alert("You Completed The game in "+ (60-time) +" seconds and with "+(8-counter) +" guesses remaining!!!" );
         location.reload();
     }
     if(counter==8){
         alert("Out of Guesses");
         location.reload();
     }
-
+  
 
         
 
@@ -59,3 +96,5 @@ window.addEventListener('click', function (e) {
 
     }
     });
+
+
